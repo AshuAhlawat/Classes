78f4d5993ae0add23ab0bec9f1cd7583a8ef58de
Shrayansh Mishra <74019038+shrayanshmishra@users.noreply.github.com>
2021-10-07 18:44:54 +0530

    Update all.py

--- all.py
+++ all.py
@@ -1,44 +1,48 @@
-people = ['ankit','ashu','swaksh']
+people = ['ankit', 'ashu', 'gaurav', 'rishika', 'sarthak', 'shrayansh', 'swaksh']
 
 
 import threading
 
-def ashu():
-    import ashu
-def swaksh():
-    import swaksh
 def ankit():
     import ankit
+def ashu():
+    import ashu
 def gaurav():
     import gaurav
+def rishika():
+    import rishika
 def sarthak():
     import sarthak
 def shrayansh():
 	import shrayansh
+def swaksh():
+    import swaksh
 
-
-ashu_class = threading.Thread(target=ashu)
-swaksh_class = threading.Thread(target=swaksh)
 ankit_class = threading.Thread(target=ankit)
+ashu_class = threading.Thread(target=ashu)
 gaurav_class = threading.Thread(target=gaurav)
+rishika_class = threading.Thread(target=rishika)
 sarthak_class = threading.Thread(target=sarthak)
 shrayansh_class = threading.Thread(target=shrayansh)
-
-
-if "ashu" in people:
-    ashu_class.start()
-
-if "swaksh" in people:
-    swaksh_class.start()
+swaksh_class = threading.Thread(target=swaksh)
 
 if "ankit" in people:
     ankit_class.start()
 
+if "ashu" in people:
+    ashu_class.start()
+
 if "gaurav" in people:
     gaurav_class.start()
+
+if "rishika" in people:
+    rishika_class.start()
 
 if "sarthak" in people:
     sarthak_class.start()
     
 if "shrayansh" in people:
     shrayansh_class.start()
+
+if "swaksh" in people:
+    swaksh_class.start()
